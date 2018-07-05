b95d98e20ebdc65bd5cc08a9377a6d9697cd89c6
vagrant <vagrant@homestead>
2018-07-05 14:02:11 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,11 @@ Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm'
 Route::post('password/reset','Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::resource('statuses','StatusesController',['only' => ['store','destroy']]);
+
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
 // Route::get('/users','UsersController@index')->name('users.index');
 // Route::get('/users/{user}','UsersController@show')->name('users.show');
 // Route::get('/users/create','UsersController@create')->name('users.create');
